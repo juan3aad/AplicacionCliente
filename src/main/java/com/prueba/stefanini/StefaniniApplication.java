95c49f25a442d3ed8fd54c3bf96783554eeecce0
juan3aad <juan3aad@gmail.com>
2021-12-22 16:23:24 -0500

    Realizando el primer commit

---
+++ src/main/java/com/prueba/stefanini/StefaniniApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.stefanini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StefaniniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StefaniniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/stefanini/StefaniniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.stefanini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StefaniniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
