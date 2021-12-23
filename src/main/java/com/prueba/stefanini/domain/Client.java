0702a6348a37b33399d3de6e24941ebaa2176ac5
Alexander Agreda <juan3aad@gmail.com>
2021-12-23 00:14:32 -0500

    Se realiza la implementacion del patrón Mapper

---
+++ src/main/java/com/prueba/stefanini/domain/Client.java
@@ -0,0 +1,49 @@
+package com.prueba.stefanini.domain;
+
+public class Client {
+    private String id;
+    private String reason;
+    private String contact;
+    private String region;
+    private String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ src/main/java/com/prueba/stefanini/domain/repository/ClientRepository.java
@@ -0,0 +1,14 @@
+package com.prueba.stefanini.domain.repository;
+
+import com.prueba.stefanini.domain.Client;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClientRepository {
+    List<Client> getAlL();
+    Optional<List<Client>> getByClient(String id);
+    Optional<Client> getClient(String id);
+    Client save(Client client);
+    void delete(String id);
+}
