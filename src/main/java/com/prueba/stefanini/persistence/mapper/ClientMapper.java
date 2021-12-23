0d3e25e70ea26027c77fc57864e4adc80a663e17
Alexander Agreda <juan3aad@gmail.com>
2021-12-23 00:25:13 -0500

    Orientamos la API al dominio con MapStruct

---
+++ src/main/java/com/prueba/stefanini/persistence/mapper/ClientMapper.java
@@ -0,0 +1,24 @@
+package com.prueba.stefanini.persistence.mapper;
+
+import com.prueba.stefanini.domain.Client;
+import com.prueba.stefanini.persistence.entity.Cliente;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface ClientMapper {
+    @Mappings({
+            @Mapping(source = "identificacion", target = "id"),
+            @Mapping(source = "razon", target = "reason"),
+            @Mapping(source = "contacto", target = "contact"),
+            @Mapping(source = "region", target = "region"),
+            @Mapping(source = "tipo", target = "type"),
+    })
+    Client toClient(Cliente cliente);
+    @InheritInverseConfiguration
+    Cliente toCliente(Client client);
+
+
+}
